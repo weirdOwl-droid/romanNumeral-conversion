3429a7931ddc702ec6a8097944b1c734e4898e27
The Owl <jenaka.puan@gmail.com>
2025-03-19 10:37:16 +0700

    "Updated Code"

--- main.go
+++ main.go
@@ -1,30 +1,23 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
+	"strconv"
 	"strings"
 )
 
-var RomanMapping = []struct {
-	value  int
-	symbol string
-}{
-	{1000, "M"},
-	{900, "CM"},
-	{500, "D"},
-	{400, "CD"},
-	{100, "C"},
-	{90, "XC"},
-	{50, "L"},
-	{40, "XL"},
-	{10, "X"},
-	{9, "IX"},
-	{5, "V"},
-	{4, "IV"},
-	{1, "I"},
-}
-
 func romanConversion(input int) (string, error) {
+	var RomanMapping = []struct {
+		value  int
+		symbol string
+	}{
+		{1000, "M"}, {900, "CM"}, {500, "D"}, {400, "CD"},
+		{100, "C"}, {90, "XC"}, {50, "L"}, {40, "XL"},
+		{10, "X"}, {9, "IX"}, {5, "V"}, {4, "IV"}, {1, "I"},
+	}
+
 	var result strings.Builder
 	if input < 1 || input > 3999 {
 		return "", fmt.Errorf("Input Error, hanya mengkonversi angka 1-3999")
@@ -35,26 +28,35 @@ func romanConversion(input int) (string, error) {
 			input -= mapping.value
 		}
 	}
-	return result.String(), fmt.Errorf("")
+	return result.String(), nil
 }
 
 func main() {
+	reader := bufio.NewReader(os.Stdin)
 
-	var input int
-	var control string
 	for {
-		fmt.Println("Silahkan masukan angka")
-		fmt.Scanln(&input)
-		fmt.Println("Hasil konversi . . .")
-		fmt.Println(romanConversion(input))
-		fmt.Println("Ingin Mengonversi angka lainnya?")
-		fmt.Println("Ketik (next) jika ingin melanjutkan")
-		fmt.Scanln(&control)
-		if control == "next" {
+		fmt.Println("Silahkan masukan angka:")
+		inputStr, _ := reader.ReadString('\n')
+		inputStr = strings.TrimSpace(inputStr)
+
+		input, err := strconv.Atoi(inputStr)
+		if err != nil {
+			fmt.Println("Error: Input tidak valid. Masukkan angka numerik.")
 			continue
+		}
+
+		if value, err := romanConversion(input); err != nil {
+			fmt.Println(err)
 		} else {
-			panic("Closing application")
+			fmt.Println("Hasil konversi:", value)
+		}
+
+		fmt.Println("Ingin Mengonversi angka lainnya? (ketik 'next' untuk melanjutkan, atau ketik apa saja untuk keluar)")
+		control, _ := reader.ReadString('\n')
+		control = strings.TrimSpace(control)
+		if control != "next" {
+			fmt.Println("Menutup aplikasi...")
+			break
 		}
 	}
-
 }
